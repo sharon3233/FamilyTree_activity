94d15e1adc36f6b00f09c27d90cae956c4be7e3c
sharon3233 <81319211+sharon3233@users.noreply.github.com>
2021-06-28 16:24:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+class Parent {
+  constructor(name, parents) {
+    this.name = name;
+    this.parents = parents;
+  }
+
+childOf(){
+  return this.parents.join(" & ");
+}
+}
+let parent1 = new Parent ('Sharon', ["Patricia", "Tony"]);
+let parent2 = new Parent ('Blance', ["Loretha", "Mary"]);
+let parent3 = new Parent ('Doretha', ["Rickey", "Kevin"]);
+
+const familyTree = [parent1,parent2,parent3];
+
+familyTree.forEach(parent =>{
+  console.log("Hi" + parent.name + "your parents are"+ parent.childOf());
+
+});
