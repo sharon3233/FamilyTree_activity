134bf1553c9c525085e43df072c02b822d240499
Kindles <kindlsh@ad.vzwcorp.com>
2021-07-01 04:36:48 -0500

    changes

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ childOf(){
   return this.parents.join(" & ");
 }
 }
-let parent1 = new Parent ('Sharon', ["Patricia", "Tony"]);
+let parent0 = new Parent ('Sharon', ["Patricia", "Tony"]);
 let parent2 = new Parent ('Blance', ["Loretha", "Mary"]);
 let parent3 = new Parent ('Doretha', ["Rickey", "Kevin"]);
 
-const familyTree = [parent1,parent2,parent3];
+const familyTree = [parent0,parent2,parent3];
 
 familyTree.forEach(parent =>{
   console.log("Hi" + parent.name + "your parents are"+ parent.childOf());
